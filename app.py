a2a3344688602d568223709d970a188f803eba0a
Besttttt <best.yamprasonk@gmail.com>
2020-12-20 04:51:28 +0700

    test03

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import logging
 
 app = Flask("My echo bot")
 
-FB_ACCESS_TOKEN = "EAA2YfX5EvmQBAAeixF2EDxrGijvj6rZCu6ZABfquFgR2ih84PUAcYdsDvye0WLwPVMsM7C1TrxHLHR4NWYcuWqB6Yv5vJsTnYv3ZBphmg1iE6pDZBRbYZBzJ4dBiVNNaFk0wVSNYOLh7xu7qGHmLwjwtI2ZC5UOgXreDaDEwhuVwZDZD"
+PAGE_ACCESS_TOKEN = "EAA2YfX5EvmQBAAeixF2EDxrGijvj6rZCu6ZABfquFgR2ih84PUAcYdsDvye0WLwPVMsM7C1TrxHLHR4NWYcuWqB6Yv5vJsTnYv3ZBphmg1iE6pDZBRbYZBzJ4dBiVNNaFk0wVSNYOLh7xu7qGHmLwjwtI2ZC5UOgXreDaDEwhuVwZDZD"
 
 
 VERIFICATION_TOKEN = "hello"
@@ -16,7 +16,6 @@ def verify():
 		if not request.args.get("hub.verify_token") == VERIFICATION_TOKEN:
 			return "Verification token mismatch", 403
 		return request.args["hub.challenge"], 200
-	print("Kuyyyyyyyyyyyyyyyyyyyyyyyyyy")
 	return "Hello world", 200
 
 @app.route('/',methods=['POST'])
