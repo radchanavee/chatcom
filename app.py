4097faa57cc6c0b0e36dc4558fb564913caa7bdd
Besttttt <best.yamprasonk@gmail.com>
2020-12-20 10:06:41 +0700

    test03

--- app.py
+++ app.py
@@ -1,53 +1,31 @@
 from flask import Flask, request
 
-app = Flask(__name__)
 
-FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
+
+app = Flask("My echo bot")
+
 PAGE_ACCESS_TOKEN = "EAA2YfX5EvmQBAAeixF2EDxrGijvj6rZCu6ZABfquFgR2ih84PUAcYdsDvye0WLwPVMsM7C1TrxHLHR4NWYcuWqB6Yv5vJsTnYv3ZBphmg1iE6pDZBRbYZBzJ4dBiVNNaFk0wVSNYOLh7xu7qGHmLwjwtI2ZC5UOgXreDaDEwhuVwZDZD"
+
+
 VERIFY_TOKEN = "hello"
 print("Testtttttt")
 
-def get_bot_response(message):
-    """This is just a dummy function, returning a variation of what
-    the user said. Replace this function with one connected to chatbot."""
-    return "This is a dummy response to '{}'".format(message)
+@app.route('/', methods=['GET'])
+def verify():
+	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+		if not request.args.get("hub.verify_token") == VERIFICATION_TOKEN:
+			return "Verification token mismatch", 403
+		return request.args["hub.challenge"], 200
+	return "Hello world", 200
+
+@app.route('/',methods=['POST'])
+def webhook():
+	data = request.get_json()
+	print(data)
+	return "ok",200
 
 
-def verify_webhook(req):
-    if req.args.get("hub.verify_token") == VERIFY_TOKEN:
-        return req.args.get("hub.challenge")
-    else:
-        return "incorrect"
+    
 
-def respond(sender, message):
-    """Formulate a response to the user and
-    pass it on to a function that sends it."""
-    response = get_bot_response(message)
-    send_message(sender, response)
-
-
-def is_user_message(message):
-    """Check if the message is a message from the user"""
-    return (message.get('message') and
-            message['message'].get('text') and
-            not message['message'].get("is_echo"))
-
-
-@app.route("/webhook")
-def listen():
-    """This is the main function flask uses to 
-    listen at the `/webhook` endpoint"""
-	print("testtttttttttttttttt")
-    if request.method == 'GET':
-        return verify_webhook(request)
-
-    if request.method == 'POST':
-        payload = request.json
-        event = payload['entry'][0]['messaging']
-        for x in event:
-            if is_user_message(x):
-                text = x['message']['text']
-                sender_id = x['sender']['id']
-                respond(sender_id, text)
-
-        return "ok"
+if __name__ == "__main__":
+	app.run(port=8000, use_reloader = True)
